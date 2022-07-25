62d96f48cc4426ebe387864205244874eeba2036
Anna <annaambrain2019@gmail.com>
2022-07-25 13:42:14 +0400

    Added leftDrawerSlice

---
+++ src/redux/index.ts
@@ -0,0 +1,17 @@
+import { configureStore, combineReducers, MiddlewareArray } from '@reduxjs/toolkit';
+import { enableMapSet } from 'immer';
+import thunk from 'redux-thunk';
+import leftDrawerSlice from './slice/leftDrawerSlice';
+
+enableMapSet();
+
+const rootReducer = combineReducers({
+    leftDrawerReducer: leftDrawerSlice,
+});
+
+export const store = configureStore({
+    reducer: rootReducer,
+    middleware: new MiddlewareArray().concat(thunk),
+});
+
+export type RootState = ReturnType<typeof rootReducer>
---
+++ src/redux/slice/leftDrawerSlice.ts
@@ -0,0 +1,18 @@
+import { createSlice, PayloadAction} from '@reduxjs/toolkit';
+
+const leftDrawerSlice = createSlice({
+    name: 'leftDrawerSlice',
+    initialState: {
+        activeTab: '',
+    },
+    reducers: {
+        setActiveTab(state, action: PayloadAction<string>) {
+            state.activeTab = action.payload;
+        }
+    }
+});
+
+export default leftDrawerSlice.reducer;
+export const {
+    setActiveTab,
+} = leftDrawerSlice.actions;
